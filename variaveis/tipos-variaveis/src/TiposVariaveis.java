16e0dab84a65cb22898b6ff946a119ec502700e8
EderdaSilva <ederanton@gmail.com>
2024-06-06 09:58:53 -0300

    tipos de variaveis

---
+++ variaveis/tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,13 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        byte idade = 123;
+        short ano = 2024;
+        int cep = 13187000;
+        long cpf = 987654321L; // Sempre que for long se finaliza com "L", indicando que não é inteiro
+        float pi = 3.14F; // Sempre que for float se deve incerrar com "F"
+        double salarioMinimo = 1400;
+       
+        
+    }
+
+}
